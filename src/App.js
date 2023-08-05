742d690c389a53c9498c96176ced28583c3f531d
mithilesh2023 <mithilesht2023@gmail.com>
2023-08-05 12:08:38 +0530

    first change

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
-
 function App() {
+  let name="Mithilesh";
+  let names="Anil";
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>My name is {name}</h1>
+      <h1 style={{color:'red',fontSize:'50px'}}>My name is {names}</h1>
+
+      {/* {statement ? 'true':'false'} */}
+    
     </div>
   );
 }
