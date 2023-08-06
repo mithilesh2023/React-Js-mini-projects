0a5fa6e87c8f267dffbf747c4598e16790b6514d
mithilesh2023 <mithilesht2023@gmail.com>
2023-08-06 22:09:03 +0530

    plus minus event done

--- src/App.js
+++ src/App.js
@@ -1,15 +1,9 @@
-import './App.css';
+import ClickEvent from './component/ClickEvent';
 function App() {
-  let name="Mithilesh";
-  let names="Anil";
   return (
-    <div className="App">
-      <h1>My name is {name}</h1>
-      <h1 style={{color:'red',fontSize:'50px'}}>My name is {names}</h1>
-
-      {/* {statement ? 'true':'false'} */}
-    
-    </div>
+    <>
+    <ClickEvent/>
+    </>
   );
 }
 
